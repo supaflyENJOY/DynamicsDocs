140441522f3f5ec967a383460c91dfe1bf6d7de8
Allie Follman <afollman@chorus.cloud>
2022-09-29 10:41:29 -0400

    update error

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,126 +1,106 @@
-// @ts-check
-// Note: type annotations allow type checking and IDEs autocompletion
-
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
-
-/** @type {import('@docusaurus/types').Config} */
-const config = {
-  title: 'Chorus Software Solutions',
-  tagline: 'At Chorus we help people help people, by creating operational efficiency, enabling the ultimate patient and practitioner (employee) experiance, resulting in highest clinical and revenue outcomes. ',
-  url: 'https://www.chorus.cloud/',
+module.exports = {
+  title: 'Note Documentation',
+  tagline: 'Your digital care companion',
+  url: 'https://notedocs.chorus.cloud/',
   baseUrl: '/',
   onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/choruslogo.png',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
+  favicon: 'img/ChorusFavicon.png',
   organizationName: 'Chorus', // Usually your GitHub org/user name.
-  projectName: 'DynamicsDocs', // Usually your repo name.
-
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+  projectName: 'Note-Documentation', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      // title: 'Chorus Documentation',
+      logo: {
+        alt: 'Chorus Logo',
+        src: 'img/ChorusFavicon.png',
+      },
+      items: [
+        {
+          to: 'docs/Overview',
+          activeBasePath: 'docs',
+          label: 'Chorus Documentation',
+          position: 'left',
+        },
+       // {to: 'blog', label: 'Blog', position: 'left'},
+        // {
+        //   href: 'https://github.com/Chorus/Note-Documentation',
+        //   label: 'GitHub',
+        //   position: 'right',
+        // },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          // title: 'Docs',
+          // items: [
+          //   {
+          //     label: 'Style Guide',
+          //     to: 'docs/',
+          //   },
+          //   {
+          //     label: 'Second Doc',
+          //     to: 'docs/doc2/',
+          //   },
+          // ],
+        },
+        {
+          // title: 'Community',
+          // items: [
+          //   {
+          //     label: 'Stack Overflow',
+          //     href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+          //   },
+          //   {
+          //     label: 'Discord',
+          //     href: 'https://discordapp.com/invite/docusaurus',
+          //   },
+          //   {
+          //     label: 'Twitter',
+          //     href: 'https://twitter.com/docusaurus',
+          //   },
+          // ],
+        },
+        {
+          // title: 'More',
+          // items: [
+          //   {
+          //     label: 'Blog',
+          //     to: 'blog',
+          //   },
+          //   {
+          //     label: 'GitHub',
+          //     href: 'https://github.com/facebook/docusaurus',
+          //   },
+          // ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Chorus Software Solutions LLC.`,
+    },
   },
-
   presets: [
     [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
+      '@docusaurus/preset-classic',
+      {
         docs: {
+          // It is recommended to set document id as docs home page (`docs/` path).
+          homePageId: 'doc1',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
+          editUrl:
+            'https://github.com/Chorus/Note-Documentation/edit/master/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
+          editUrl:
+            'https://github.com/Chorus/Note-Documentation/edit/master/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
-      }),
+      },
     ],
   ],
-
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: 'Chorus',
-        logo: {
-          alt: 'Chorus Logo',
-          src: 'img/choruslogo.png',
-        },
-        items: [
-          {
-            type: 'doc',
-            docId: 'Overview/IntakeRepLinks',
-            position: 'left',
-            label: 'Documentation',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
 };
-
-module.exports = config;
