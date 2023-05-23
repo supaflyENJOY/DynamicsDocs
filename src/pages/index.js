236d4ed0334841f2484aaae039510ab1ec6e7519
Miriam Leff <mleff@chorus.cloud>
2023-05-23 16:46:42 -0400

    overview link fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
       <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/Overview/Welcome.md/">
+            to="/docs/Overview/Welcome">
             Get Started
           </Link>
       </div>
